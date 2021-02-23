ec6261adb6cded28cb1237fe66d6091b1de220b7
ankith26 <46915066+ankith26@users.noreply.github.com>
2021-02-23 21:31:41 +0530

    upgrade build script

--- build.py
+++ build.py
@@ -1,12 +1,12 @@
 """
 Builder script to build Kithare.
 
-On Windows and MINGW:
-    You must have MINGW (AKA MinGW-w64) installed, and 'g++' command must
+On Windows and MinGW:
+    You must have MinGW (AKA MinGW-w64) installed, and 'g++' command must
     be on PATH.
 
-    By default, this compiles for 64-bit architectures. If you want to compile
-    for 32-bit architecture, pass '-m32' as an argument to the build script.
+    If you are on a 64-bit system, and want to compile for 32-bit architecture,
+    pass '-m32' as an argument to the build script.
 
     This builder automatically installs SDL dependencies. Just run this file
     with: 'py build.py'.
@@ -16,7 +16,15 @@ On Windows and MSVC:
     job in this case, that is installing SDL dependencies. Just run:
     'py build.py --msvc'
 
-On Other OS:
+On Mac:
+    (Unfinished as of now)
+    This assumes you have clang (clang++) installed. Also, you need to install 
+    SDL dependencies on your own.
+    Kithare needs 'SDL2', 'SDL2_mixer', 'SDL2_image', 'SDL2_ttf' and 'SDL2_net'.
+    Make sure to install 'devel' releases of those, not just runtime shared 
+    libraries.
+
+On other OS:
     This assumes you have GCC (and G++) installed. Also, you need to install SDL
     dependencies on your own, via your systems package manager.
     Kithare needs 'SDL2', 'SDL2_mixer', 'SDL2_image', 'SDL2_ttf' and 'SDL2_net'.
@@ -31,7 +39,6 @@ doing.
 """
 
 import glob
-import hashlib
 import io
 import os
 import platform
@@ -41,17 +48,12 @@ import tarfile
 import urllib.request as urllib
 import zipfile
 
-WIN = platform.system() == "Windows"
+EXE = "kcr.exe" if platform.system() == "Windows" else "kcr"
 
-DIST = "dist/kcr"
-if WIN:
-    DIST += ".exe"
-
-cflags = "-O3 -I include"
-cflags += " -lSDL2 -lSDL2main -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lSDL2_net"
-
-if WIN:
-    cflags += " -municode"
+# While we recursively search for include files, we don't want to seach
+# the whole file, because that would waste a lotta time. So, we just take
+# an arbitrary line number limit, beyond which, we won't search
+INC_FILE_LINE_LIMIT = 26
 
 # SDL project-version pairs, remember to keep updated
 SDL_DEPS = {
@@ -62,33 +64,43 @@ SDL_DEPS = {
     "SDL2_net": "2.0.1",
 }
 
-# While we recursively search for include files, we don't want to seach
-# the whole file, because that would waste a lotta time. So, we just take
-# an arbitrary line number limit, beyond which, we won't search
-INC_FILE_LINE_LIMIT = 26
+is_32_bit_req = "-m32" in sys.argv
 
-IS_32_BIT = "-m32" in sys.argv
-IS_MSVC = "--msvc" in sys.argv
+_machine = platform.machine()
+if _machine in ["i686", "i386"]:
+    machine = "x86"
+    machine_alt = "i686"
 
-if not IS_32_BIT:
-    IS_32_BIT = sys.maxsize == 2 ** 32 - 1
+elif _machine.lower() in ["x86_64", "amd64"]:
+    machine = "x86" if is_32_bit_req else "x64"
+    machine_alt = "i686" if is_32_bit_req else "x86_64"
 
-ARCH = 'x86' if IS_32_BIT else 'x64'
+elif _machine.lower() in ["arm", "arm32", "armv6l", "armv7l"]:
+    machine = "ARM"
+
+elif _machine.lower() in ["armv8l", "arm64", "aarch64"]:
+    machine = "ARM" if is_32_bit_req else "ARM64"
+
+else:
+    machine = _machine if _machine else "None"
+
+if platform.system() == "Windows":
+    compiler = "MSVC" if "--msvc" in sys.argv else "MinGW"
+
+else:
+    compiler = "Clang" if platform.system() == "Darwin" else "GCC"
+
+cc = "clang++" if compiler == "Clang" else "g++"
+
+cflags = "-O3 -I include"
+cflags += " -lSDL2 -lSDL2main -lSDL2_image -lSDL2_ttf -lSDL2_mixer -lSDL2_net"
+
+if compiler == "MinGW":
+    cflags += " -municode"
 
 cflags += " " + " ".join(sys.argv[1:])
 
-DOWNLOAD_DIR = f"deps/SDL-{'MSVC' if IS_MSVC else 'mingw'}"
-
-if IS_MSVC:
-    INC_DIR = "include"
-    LIB_DIR = f"lib/{ARCH}"
-    DLL_DIR = LIB_DIR
-    
-else:
-    _GPP_ARCH = "i686" if IS_32_BIT else "x86_64"
-    DLL_DIR = f"{_GPP_ARCH}-w64-mingw32/bin"
-    LIB_DIR = f"{_GPP_ARCH}-w64-mingw32/lib"
-    INC_DIR = f"{_GPP_ARCH}-w64-mingw32/include/SDL2"
+download_dir = f"deps/SDL-{compiler}"
 
 
 def mkdir(file):
@@ -108,8 +120,10 @@ def find_includes(file):
                 retfile = line[len("#include "):]
                 for char in ['"', '<', '>']:
                     retfile = retfile.replace(char, "")
-                retfile = os.path.join("include", 
-                    os.path.normcase(retfile.strip()))
+                retfile = os.path.join(
+                    "include", 
+                    os.path.normcase(retfile.strip())
+                )
 
                 if os.path.isfile(retfile):
                     yield retfile
@@ -145,9 +159,9 @@ def download_sdl_deps(name, version):
         download_link += f"projects/{name}/".replace("2", "")
 
     download_link += f"release/{name}-devel-{version}-"
-    download_link += "VC.zip" if IS_MSVC else "mingw.tar.gz"
+    download_link += "VC.zip" if compiler == "MSVC" else "mingw.tar.gz"
 
-    download_path = f"{DOWNLOAD_DIR}/{name}"
+    download_path = f"{download_dir}/{name}"
 
     if os.path.isdir(download_path):
         print(f"Skipping {name} download because it already exists")
@@ -163,9 +177,9 @@ def download_sdl_deps(name, version):
             response = download.read()
 
         print("Extracting compressed files")
-        if IS_MSVC:
+        if compiler == "MSVC":
             with zipfile.ZipFile(io.BytesIO(response), 'r') as zipped:
-                zipped.extractall(DOWNLOAD_DIR)
+                zipped.extractall(download_dir)
 
         else:
             # Tarfile does not support bytes IO, so use temp file
@@ -174,27 +188,45 @@ def download_sdl_deps(name, version):
                     f.write(response)
 
                 with tarfile.open("temp", 'r:gz') as tarred:
-                    tarred.extractall(DOWNLOAD_DIR)
+                    tarred.extractall(download_dir)
             finally:
                 if os.path.exists("temp"):
                     os.remove("temp")
         
         os.rename(f"{download_path}-{version}", download_path)
-
-        # Copy all SDL DLLs to dist dir
-        for dll in glob.iglob(f"{download_path}/{DLL_DIR}/*.dll"):
-            shutil.copyfile(dll, os.path.join("dist", os.path.basename(dll)))
-
         print(f"Finished downloading {name}")
 
-    return f" -I {download_path}/{INC_DIR} -L {download_path}/{LIB_DIR}"
+    # Copy all SDL DLLs to dist dir
+    if compiler == "MSVC":
+        for i in ["x86", "x64"]:
+            for j in ["Debug", "Release"]:
+                d = f"dist/MSVC-{i}-{j}"
+                mkdir(d)
+                mkdir(f"build/MSVC-{i}-{j}")
+                for dll in glob.iglob(f"{download_path}/lib/{i}/*.dll"):
+                    shutil.copyfile(
+                        dll, 
+                        os.path.join(d, os.path.basename(dll))
+                    )
+        return ""
+
+    else:
+        for dll in glob.iglob(
+            f"{download_path}/{machine_alt}-w64-mingw32/bin/*.dll"
+        ):
+            shutil.copyfile(
+                dll, 
+                os.path.join(f"dist/MinGW-{machine}", os.path.basename(dll))
+            )
+        return f" -I {download_path}/{machine_alt}-w64-mingw32/include/SDL2" + \
+            f" -L {download_path}/{machine_alt}-w64-mingw32/lib"
 
 
 def compile_gpp(src, output, srcflag=""):
     """
     Used to execute g++ commands
     """
-    cmd = f"g++ -o {output} {srcflag}{src} {cflags}"
+    cmd = f"{cc} -o {output} {srcflag}{src} {cflags}"
     print(cmd)
     return os.system(cmd)
 
@@ -205,15 +237,27 @@ def main():
     """
     global cflags
 
-    mkdir(os.path.dirname(DIST))
-    if WIN:
-        mkdir(DOWNLOAD_DIR)
+    builddir = f"build/{compiler}-{machine}"
+    distdir = f"dist/{compiler}-{machine}"
+    if compiler != "MSVC":
+        mkdir(builddir)
+        mkdir(distdir)
+
+    # Prepare dependencies
+    if platform.system() == "Windows":
+        mkdir(download_dir)
+
         for package, ver in SDL_DEPS.items():
             cflags += download_sdl_deps(package, ver)
 
-        if IS_MSVC:
+        if compiler == "MSVC":
             print("Done!")
             return
+
+    elif platform.system() == "Darwin":
+        # TODO: Include SDL directories properly on this one
+        pass
+
     else:
         for inc_dir in ["/usr/include/SDL2", "/usr/local/include/SDL2"]:
             if os.path.isdir(inc_dir):
@@ -221,12 +265,10 @@ def main():
                 break
     
     print()
-    mkdir("build")
     isfailed = False
     for file in glob.iglob("src/**/*.cpp", recursive=True):
-        ofile = file.replace("src", "build", 1).rstrip(".cpp") + f"-{ARCH}.o"
+        ofile = f"{builddir}/{os.path.basename(file)}".replace(".cpp", ".o")
         if should_build(file, ofile):
-            mkdir(os.path.dirname(ofile))
             print("Building file:", file)
             if compile_gpp(file, ofile, "-c "):
                 print("g++ command exited with an error")
@@ -234,26 +276,21 @@ def main():
             print()
     
     if isfailed:
-        print("Skipped building final executable, because all files didn't build")
+        print("Skipped building executable, because all files didn't build")
         return
     
-    if not os.path.exists(DIST):
+    if not os.path.exists(f"{distdir}/{EXE}"):
         print("Building exe")
-        compile_gpp(
-            " ".join(
-                glob.iglob(f"build/**/*-{ARCH}.o", recursive=True)
-            ),
-            DIST
-        )
+        compile_gpp(" ".join(glob.iglob(f"{builddir}/*.o")), f"{distdir}/{EXE}")
 
     else:
-        dist_m = os.stat(DIST).st_mtime
-        obj_files = glob.glob(f"build/**/*-{ARCH}.o", recursive=True)
+        dist_m = os.stat(f"{distdir}/{EXE}").st_mtime
+        obj_files = glob.glob(f"{builddir}/*.o")
 
         for ofile in obj_files:
             if os.stat(ofile).st_mtime > dist_m:
                 print("Building exe")
-                compile_gpp(" ".join(obj_files), DIST)
+                compile_gpp(" ".join(obj_files), f"{distdir}/{EXE}")
                 break
     print("Done!")
 
