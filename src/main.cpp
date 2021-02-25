114a19badb4a96ba87571e71d6b442ce0cf1876d
ankith26 <46915066+ankith26@users.noreply.github.com>
2021-02-25 10:11:53 +0530

    remove useless bit of code

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,6 @@ int main(const int argc, char* argv[])
     std::vector<kh::String> args;
 
     #ifdef _WIN32
-    std::ios_base::sync_with_stdio(false);
     std::locale utf8(std::locale(), new std::codecvt_utf8_utf16<wchar_t>);
     std::wcout.imbue(utf8);
 
