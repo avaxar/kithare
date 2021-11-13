f5cf6c3d6f0d75e90713510c6d5ddeaa20a2a8ab
AvaxarXapaxa <44055981+AvaxarXapaxa@users.noreply.github.com>
2021-11-13 16:30:52 +0700

    Finish lexer (Untested)

--- src/cli.c
+++ src/cli.c
@@ -15,20 +15,7 @@
 #include <kithare/lexer.h>
 
 
-void cli() {
-    char str[] = "12.3456e2";
-    char* str_ptr = &str[0];
-    printf("%f\n", kh_lexFloat(&str_ptr, 10));
-
-    char utf8[] = "\x42\x63\xc3\xb6\xc3\xb3\xe4\x89\x82\xe6\x8d\xa3\xf0\x90\x80\x80";
-    // int32_t utf32[] = {0x42, 0x63, 0xf6, 0xf3, 0x4242, 0x6363, 0x10000, 0x0};
-    str_ptr = &utf8[0];
-
-    int32_t c;
-    while ((c = kh_lexUtf8(&str_ptr)) != 0) {
-        printf("%d\n", c);
-    }
-}
+void cli() {}
 
 
 // Entry point of the Kithare CLI program
